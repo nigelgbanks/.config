9b6c9b4523124601df7e237a18ec30cdd1cf8036
Nigel Banks <nigel.g.banks@gmail.com>
2013-05-04 20:31:46 +0200

    Have debian fetch the lastest stable version of git.

--- bash/setup_debian.sh
+++ bash/setup_debian.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# First make sure what we have is the latest.
 sudo apt-get update
 sudo apt-get upgrade
+
+# Get the most recent version of git.
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
 sudo apt-get install git
