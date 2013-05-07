1600792997615084ca3b7373b57de68122a6bc57
Nigel Banks <nigel.g.banks@gmail.com>
2013-05-07 22:49:51 +0200

    Get latest git on CentOS.

--- bash/setup_redhat.sh
+++ bash/setup_redhat.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# Add the repository
+sudo rpm -Uvh http://repo.webtatic.com/yum/centos/5/latest.rpm
+
+# Install the latest version of git
+sudo yum install --enablerepo=webtatic git-core
